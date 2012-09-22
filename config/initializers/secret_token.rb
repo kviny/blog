7845759ed3178139e23071aef2486af553eb4bc7
kviny <kviny@ubuntu.ubuntu-domain>
2012-09-22 20:03:41 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8ba18257d516086851132a90200fa637b1f9ea0bdcdd93677cf71688af24c3fbfcff83aa3971b0d1f0e2502ac66afdba5e19b8991dbd2a8b71d02fcb8887adb9'
